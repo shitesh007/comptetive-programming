a23d77cd2c80bb8f25f6efb2c79f25b3181ed951
shitesh kumar <sitesh9334@gmail.com>
2026-01-21 14:50:43 +0530

    add lecture 02 problems

---
+++ lecture 02/problem03.cpp
@@ -0,0 +1,22 @@
+// * * * * *  
+// * * * *  
+// * * *  
+// * *  
+// *
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+           cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
